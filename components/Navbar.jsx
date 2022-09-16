ced1440338740a1a15686683a46377cb0127b586
nvcho <cu.ouza@gmail.com>
2022-09-16 16:24:12 -0300

    [ADD] connect button with rainbow

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,20 +6,26 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 const Navbar = () => {
 	return (
-		<nav className="px-4 flex justify-between">
+		<nav className="flex justify-between px-4">
 			<Image src="/wooylogo.svg" width={150} height={150} />
 			<div className="py-12">
-				<BorderWrapper size={"w-[175px] h-[40px]"} className={"font-bold"}>
-					CONNECT WALLET
-				</BorderWrapper>
-				
+				<ConnectButton.Custom>
+					{({ openConnectModal }) => {
+						return (
+							<button onClick={openConnectModal}>
+								<BorderWrapper
+									size={'w-[175px] h-[40px]'}
+									className={'font-bold'}
+								>
+									CONNECT WALLET
+								</BorderWrapper>
+							</button>
+						)
+					}}
+				</ConnectButton.Custom>
 			</div>
 		</nav>
 	)
 }
 
 export default Navbar
-													{/* <ConnectButton /> */}
-					{/* <div className="top-[0.5rem] left-[0.5rem] absolute w-[175px] h-[45px] bg-white rounded-lg text-black flex items-center justify-center font-semibold shadow-[5px_5px_1px_rgba(0,0,0,1)] "></div>
-					CONNECT WALLET */}
-				{/* <div className=" w-[175px] h-[45px] bg-white rounded-lg text-black flex items-center justify-center font-semibold shadow-[5px_5px_1px_rgba(255,0,0,1)] "></div> */}
--- pages/_app.js
+++ pages/_app.js
@@ -1,80 +1,85 @@
-import "@rainbow-me/rainbowkit/styles.css";
+import '@rainbow-me/rainbowkit/styles.css'
 
-import "../styles/globals.css";
+import '../styles/globals.css'
 
 import {
-  apiProvider,
-  configureChains,
-  getDefaultWallets,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
+	apiProvider,
+	configureChains,
+	darkTheme,
+	getDefaultWallets,
+	RainbowKitProvider,
+} from '@rainbow-me/rainbowkit'
 
-import { createClient, WagmiProvider } from "wagmi";
+import { createClient, WagmiProvider } from 'wagmi'
 
 const mumbaiTestnet = {
-  id: 80001,
-  name: "Mumbai Testnet",
-  nativeCurrency: {
-    decimals: 18,
-    name: "MATIC",
-    symbol: "MATIC",
-  },
-  rpcUrls: {
-    default: "https://rpc-mumbai.maticvigil.com",
-  },
-  blockExplorers: {
-    default: {
-      name: "Mumbai Explorer",
-      url: "https://mumbai.polygonscan.com/",
-    },
-  },
-  testnet: true,
-};
-
-const polygon = {
-  id: 137,
-  name: "Polygon",
-  nativeCurrency: {
-    decimals: 18,
-    name: "MATIC",
-    symbol: "MATIC",
-  },
-  rpcUrls: {
-    default: "https://polygon-rpc.com",
-  },
-  blockExplorers: {
-    default: {
-      name: "Polygon Explorer",
-      url: "https://polygonscan.com/",
-    },
-  },
-  testnet: false,
-};
-
-const { chains, provider } = configureChains(
-  [mumbaiTestnet, polygon],
-  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "Wooy",
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <WagmiProvider client={wagmiClient}>
-      <RainbowKitProvider showRecentTransactions={true} chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiProvider>
-  );
+	id: 80001,
+	name: 'Mumbai Testnet',
+	nativeCurrency: {
+		decimals: 18,
+		name: 'MATIC',
+		symbol: 'MATIC',
+	},
+	rpcUrls: {
+		default: 'https://rpc-mumbai.maticvigil.com',
+	},
+	blockExplorers: {
+		default: {
+			name: 'Mumbai Explorer',
+			url: 'https://mumbai.polygonscan.com/',
+		},
+	},
+	testnet: true,
 }
 
-export default MyApp;
+const polygon = {
+	id: 137,
+	name: 'Polygon',
+	nativeCurrency: {
+		decimals: 18,
+		name: 'MATIC',
+		symbol: 'MATIC',
+	},
+	rpcUrls: {
+		default: 'https://polygon-rpc.com',
+	},
+	blockExplorers: {
+		default: {
+			name: 'Polygon Explorer',
+			url: 'https://polygonscan.com/',
+		},
+	},
+	testnet: false,
+}
+
+const { chains, provider } = configureChains(
+	[mumbaiTestnet, polygon],
+	[apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+)
+
+const { connectors } = getDefaultWallets({
+	appName: 'Wooy',
+	chains,
+})
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+})
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<WagmiProvider client={wagmiClient}>
+			<RainbowKitProvider
+				showRecentTransactions={true}
+				chains={chains}
+				theme={darkTheme()}
+			>
+				<Component {...pageProps} />
+			</RainbowKitProvider>
+		</WagmiProvider>
+	)
+}
+
+export default MyApp
