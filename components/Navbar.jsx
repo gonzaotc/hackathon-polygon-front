4d9a5c3889e8cd1f770d0ed6b9711cd3d4335960
nvcho <cu.ouza@gmail.com>
2022-09-18 17:55:58 -0300

    [ADD] connect button finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,31 +3,39 @@ import Image from 'next/image'
 import BorderWrapper from './Wrappers/BorderWrapper'
 
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useAccount } from 'wagmi'
 
 const Navbar = () => {
+	const { isConnected } = useAccount()
+
 	return (
 		<nav className="flex justify-between px-4">
-			<Image src="/wooylogo.svg" width={150} height={150} />
+			<Image
+				src="/wooylogo.svg"
+				width={150}
+				height={150}
+				alt="wooy logo"
+			/>
+
 			<div className="py-12">
-				<ConnectButton.Custom>
-					{({ openConnectModal }) => {
-						return (
-							<button onClick={openConnectModal}>
-								<BorderWrapper
-									className={'h-[45px] w-[175px] font-bold'}
-									rounded="rounded-xl"
-									position="bottom-1.25 right-1.25"
-									contraPosition="top-1.25 left-1.25"
-									outline="outline outline-[3px] outline-black"
-								>
-									<div className="flex h-full w-full items-center justify-center rounded-2lg bg-white">
-										CONNECT WALLET
-									</div>
-								</BorderWrapper>
-							</button>
-						)
-					}}
-				</ConnectButton.Custom>
+				{isConnected ? (
+					<ConnectButton />
+				) : (
+					<BorderWrapper
+						className={'h-[45px] w-[175px] font-bold'}
+						rounded="rounded-xl"
+						position="bottom-1.25 right-1.25"
+						contraPosition="top-1.25 left-1.25"
+						outline="outline outline-[3px] outline-black"
+					>
+						<div
+							id="connectButtonStyle"
+							className="flex h-full w-full"
+						>
+							<ConnectButton />
+						</div>
+					</BorderWrapper>
+				)}
 			</div>
 		</nav>
 	)
